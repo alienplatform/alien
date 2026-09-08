2c8cbb0a65177e53d31f5d2da08b6b79b85f9e84
Alon Gubkin <alon.gubkin@gmail.com>
2026-09-08 01:34:28 -0700

    fix: reject unknown named permission sets before deployment

--- crates/alien-preflights/src/lib.rs
+++ crates/alien-preflights/src/lib.rs
@@ -355,6 +355,7 @@ impl PreflightRegistry {
             compile_time::sandbox_platform_support::SandboxPlatformSupportCheck,
         ));
         registry.add_compile_time_check(Box::new(compile_time::PermissionProfilesExistCheck));
+        registry.add_compile_time_check(Box::new(compile_time::PermissionSetsExistCheck));
         registry.add_compile_time_check(Box::new(compile_time::SingleExposedPortCheck));
         registry.add_compile_time_check(Box::new(compile_time::ResourceNameLengthCheck));
         registry.add_compile_time_check(Box::new(compile_time::ResourceIdPatternCheck));
